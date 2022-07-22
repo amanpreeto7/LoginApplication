57bcf63457544dd7a8336d9ee774ad5349238587
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-07-22 12:35:37 +0530

    Commit
    Imageview added
    Login button if else conditions added
    TextView Drawable added

--- app/src/main/java/com/o7services/loginapplication/MainActivity.kt
+++ app/src/main/java/com/o7services/loginapplication/MainActivity.kt
@@ -2,10 +2,43 @@ package com.o7services.loginapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName: EditText
+    lateinit var etPassword: EditText
+    lateinit var tvForgotPassword: TextView
+    lateinit var btnLogin: Button
+    lateinit var ivAdd: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etName = findViewById(R.id.etName)
+        etPassword = findViewById(R.id.etPassword)
+        tvForgotPassword = findViewById(R.id.tvForgotPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        ivAdd = findViewById(R.id.ivAdd)
+
+        ivAdd.setOnClickListener {
+            System.out.println("Image Clicked")
+        }
+
+        btnLogin.setOnClickListener {
+            System.out.println("Clicked")
+            var name = etName.text.toString()
+            var password = etPassword.text.toString()
+            System.out.println("name $name")
+            if(name.isNullOrEmpty()){
+                etName.error = resources.getString(R.string.please_enter_name)
+                etName.requestFocus()
+            }else if(password.isNullOrEmpty()){
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+            }else{
+                Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
