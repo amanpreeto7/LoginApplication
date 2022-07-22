cc3e6fdc8a40a7c24119b91c57b1c679a4532076
Amanpreet <78461129+o7services025@users.noreply.github.com>
2022-07-22 11:44:43 +0530

    changes

--- app/src/main/java/com/o7services/loginapplication/MainActivity.kt
+++ app/src/main/java/com/o7services/loginapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var arrayVariable = arrayOf(1,2,3)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
