dc80beacd4878e3cd14ef12d4372f1d26f783283
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-07-23 14:43:51 +0530

    Radio button +Intent + Scroll View
    Radio group and button code added
    Moved from one screen to another

--- app/src/main/java/com/o7services/loginapplication/MainActivity.kt
+++ app/src/main/java/com/o7services/loginapplication/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.o7services.loginapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     lateinit var etName: EditText
     lateinit var etPassword: EditText
     lateinit var tvForgotPassword: TextView
     lateinit var btnLogin: Button
-    lateinit var ivAdd: ImageView
+    lateinit var ivNotification: ImageView
+    lateinit var rgGender: RadioGroup
+    lateinit var rbHe: RadioButton
+    lateinit var rbShe: RadioButton
+    lateinit var rbOther: RadioButton
+    lateinit var etOtherName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,13 +26,52 @@ class MainActivity : AppCompatActivity() {
         etPassword = findViewById(R.id.etPassword)
         tvForgotPassword = findViewById(R.id.tvForgotPassword)
         btnLogin = findViewById(R.id.btnLogin)
-        ivAdd = findViewById(R.id.ivAdd)
+        ivNotification = findViewById(R.id.ivNotification)
+        etOtherName = findViewById(R.id.etOtherName)
 
-        ivAdd.setOnClickListener {
-            System.out.println("Image Clicked")
+        etPassword.doOnTextChanged { text, _, _, _ ->
+            if((text?.length ?:0) <6){
+                etPassword.error = resources.getString(R.string.please_enter_password)
+            }else{
+                etPassword.error = null
+            }
         }
 
+        //radio group and button started 23 July, 2022
+        rbHe = findViewById(R.id.rbHe)
+        rbShe = findViewById(R.id.rbShe)
+        rbOther = findViewById(R.id.rbOther)
+        rgGender = findViewById(R.id.rgGender)
+
+        rgGender.setOnCheckedChangeListener { radioGroup, id ->
+            when(id){
+                R.id.rbOther->{
+                    Toast.makeText(this, resources.getString(R.string.others), Toast.LENGTH_LONG).show()
+                    etOtherName.visibility = View.VISIBLE
+                }
+                else->{
+                    etOtherName.visibility = View.INVISIBLE
+                }
+            }
+        }
+        //handle individual radio button clicks
+      /*  rbOther.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                etOtherName.visibility = View.VISIBLE
+            }else{
+                etOtherName.visibility = View.INVISIBLE
+            }
+        }*/
+
+        //radio group and button code ended
+
+        ivNotification.setOnClickListener {
+            Toast.makeText(this, resources.getString(R.string.notification_clicked), Toast.LENGTH_LONG).show()
+        }
         btnLogin.setOnClickListener {
+            //to clear the text
+            etName.text.clear()
+
             System.out.println("Clicked")
             var name = etName.text.toString()
             var password = etPassword.text.toString()
@@ -34,8 +82,15 @@ class MainActivity : AppCompatActivity() {
             }else if(password.isNullOrEmpty()){
                 etPassword.error = resources.getString(R.string.please_enter_password)
                 etPassword.requestFocus()
-            }else{
+            }else if(password.length<6){
+                etPassword.error = resources.getString(R.string.please_enter_password)
+                etPassword.requestFocus()
+            }
+            else{
                 Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_LONG).show()
+                var intent = Intent(this, ForgotPasswordActivity::class.java)
+                startActivity(intent)
+                finish()
             }
 
         }
