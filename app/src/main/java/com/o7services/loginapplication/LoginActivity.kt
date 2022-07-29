5f8cea1b8878e915d293b2773b82a280d3331aca
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-07-29 10:41:12 +0530

    image set from activity

--- app/src/main/java/com/o7services/loginapplication/LoginActivity.kt
+++ app/src/main/java/com/o7services/loginapplication/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.o7services.loginapplication
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +14,7 @@ class LoginActivity : AppCompatActivity() {
     lateinit var etPhone: EditText
     lateinit var btnLogin : Button
     lateinit var tvForgotPassword : TextView
+    lateinit var ivNotification : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +23,8 @@ class LoginActivity : AppCompatActivity() {
         etPhone = findViewById(R.id.etPhoneNumber)
         btnLogin = findViewById(R.id.btnLogin)
         tvForgotPassword = findViewById(R.id.tvForgotPassword)
-
+        ivNotification = findViewById(R.id.ivNotification)
+        ivNotification.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_add))
         tvForgotPassword.setOnClickListener {
             var intent = Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
@@ -34,7 +35,11 @@ class LoginActivity : AppCompatActivity() {
             }
         }
         btnLogin.setOnClickListener {
-            if(etName.text.toString().isNullOrEmpty()){
+            System.out.println("Login clicked")
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.google.com/")
+            startActivity(openURL)
+           /* if(etName.text.toString().isNullOrEmpty()){
                 Toast.makeText(this, resources.getString(R.string.please_enter_name), Toast.LENGTH_LONG).show()
                 etName.error = resources.getString(R.string.please_enter_name)
                 etName.requestFocus()
@@ -42,7 +47,7 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, resources.getString(R.string.please_enter_password), Toast.LENGTH_LONG).show()
                 etPassword.error = resources.getString(R.string.please_enter_password)
                 etPassword.requestFocus()
-            }
+            }*/
         }
 
     }
