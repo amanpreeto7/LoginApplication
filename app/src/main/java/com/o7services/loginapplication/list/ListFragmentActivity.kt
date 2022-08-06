d8a83f4f84d396fb3bbed157037bb6ce3f9979fe
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-08-06 09:44:29 +0530

    list fragment added

---
+++ app/src/main/java/com/o7services/loginapplication/list/ListFragmentActivity.kt
@@ -0,0 +1,12 @@
+package com.o7services.loginapplication.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.o7services.loginapplication.R
+
+class ListFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_fragment_activityt)
+    }
+}
